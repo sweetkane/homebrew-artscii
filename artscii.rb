df1adad36efc2c290da30d5991828fcc6cd4a6ef
Kane Sweet <kanesweet11@gmail.com>
2024-03-15 17:00:55 -0500

    update formula

--- artscii.rb
+++ artscii.rb
@@ -1,15 +1,22 @@
 class Artscii < Formula
+  include Language::Python::Virtualenv
+
   desc "This tool uses AI to generate ascii art from a given prompt."
   homepage "https://github.com/sweetkane/artscii"
-  url "https://github.com/sweetkane/artscii/releases/download/v0.0.1/artscii-0.0.1.tar.gz"
-  sha256 "tarball_sha256"
+  url "https://github.com/sweetkane/artscii/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "27423197e10e59fdf16a53c6e174217c81f059ec9178a426a73cc35ea17b23f3"
   license "Apache-2.0"
 
   depends_on "TheZoraiz/ascii-image-converter/ascii-image-converter"
   depends_on "python@3.12"
 
-  def install
-    bin.install "artscii"
+  resource "requests" do 
+    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+  end
+  
+    def install
+    bin.install "artscii.py" => "artscii"
   end
 
   test do
