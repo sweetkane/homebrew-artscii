c4d9fe131bb88008fa0f2435dfb33580f39fb48e
Kane Sweet <kanesweet11@gmail.com>
2024-03-18 10:41:41 -0500

    add all dependencies to formula

--- artscii.rb
+++ artscii.rb
@@ -4,19 +4,39 @@ class Artscii < Formula
   desc "This tool uses AI to generate ascii art from a given prompt."
   homepage "https://github.com/sweetkane/artscii"
   url "https://github.com/sweetkane/artscii/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "27423197e10e59fdf16a53c6e174217c81f059ec9178a426a73cc35ea17b23f3"
+  sha256 "07e543ea008d29212697b985fc8cbf4fd989f09a1422ef79bcfd558f0a2617f3"
   license "Apache-2.0"
 
   depends_on "TheZoraiz/ascii-image-converter/ascii-image-converter"
   depends_on "python@3.12"
 
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/37/f7/2b1b0ec44fdc30a3d31dfebe52226be9ddc40cd6c0f34ffc8923ba423b69/certifi-2022.12.7.tar.gz"
+    sha256 "35824b4c3a97115964b408844d64aa14db1cc518f6562e8d7261699d1350a9e3"
+  end
+  
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/96/d7/1675d9089a1f4677df5eb29c3f8b064aa1e70c1251a0a8a127803158942d/charset-normalizer-3.0.1.tar.gz"
+    sha256 "ebea339af930f8ca5d7a699b921106c6e29c617fe9606fa7baa043c1cdae326f"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
+    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
+  end
+
   resource "requests" do 
     url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
     sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
   end
-  
-    def install
-    bin.install "artscii.py" => "artscii"
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/c5/52/fe421fb7364aa738b3506a2d99e4f3a56e079c0a798e9f4fa5e14c60922f/urllib3-1.26.14.tar.gz"
+    sha256 "076907bf8fd355cde77728471316625a4d2f7e713c125f51953bb5b3eecf4f72"
+  end
+
+  def install
+    virtualenv_install_with_resources
   end
 
   test do
